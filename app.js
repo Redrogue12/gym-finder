1af658843f2d93a777955f493ac1788aa4e65be0
Redrogue12 <exgc12@gmail.com>
2017-05-11 11:56:46 -0400

    eliminated unnecesary functions

--- app.js
+++ app.js
@@ -14,34 +14,6 @@ function getDataFromApi(location, callback) {
     $.getJSON(FOURSQUARE_URL, query, callback)
 }
 
-function getPhotoFromApi(url, venueID, callback) {
-    FOURSQUARE_PHOTO_URL += venueID + '/photos/'
-    var query = {
-    client_id: 'OKKLGV0OHNRNEF1B04HEOHQJRGVHZWY4RLWNWZWMR1U2NV3C',
-    client_secret: '2RGFKZTMXUCLFZLUOQJNBZW0KXLNPSPSMUYWJJCK4JQZKMJB',
-    v: 20170401
-    }
-    $.getJSON(FOURSQUARE_PHOTO_URL, query, callback)
-}
-
-function displayPhoto(data) {
-
-
-    if (data.response.photos.items.length > 0) {
-        var prefix = data.response.photos.items[0].prefix
-        var suffix = data.response.photos.items[0].suffix
-        var imageLink = prefix + '200x200' + suffix
-        console.log(imageLink);
-        var imageElement = "<img src='" + imageLink + "'></img>"
-        return imageElement
-    } else {
-
-        return "<img src='images/noPhoto.png'></img>"
-    }
-
-
-}
-
 function displaySearchData(data) {
     var venueID = ''
     var i = 0
